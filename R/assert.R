5515b796b0a5d1e5e326f11f710c1638e169d3d2
nndey <79761317+nndey@users.noreply.github.com>
2024-10-19 19:50:15 -0400

    Update assert.R

--- R/assert.R
+++ R/assert.R
@@ -17,8 +17,9 @@ has.mappoly2.rf <- function(x){
 data.has.genome.info <- function(x){
   is.mappoly2.data(x) & !all(is.na(x$genome.pos))
 }
-is.mappoly2.sequence <- function (x)
-  inherits(x, "mappoly2.sequence")
+is.mappoly2.sequence <- function(x) {
+  return(inherits(x, "mappoly2.sequence"))
+}
 
 is.phased.sequence <- function(x){
   is.mappoly2.sequence(x) &&
