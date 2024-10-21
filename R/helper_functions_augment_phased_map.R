9b8b3e3544b7c6bf0a8e10911a6fdd2643adfc2c
nndey <79761317+nndey@users.noreply.github.com>
2024-10-21 18:46:38 -0400

    Create helper_functions_augment_phased_map.R

---
+++ R/helper_functions_augment_phased_map.R
@@ -0,0 +1,29 @@
+# helper_functions_augment_phased_map.R
+
+phasing_one <- function(...) {
+  # Function body for phasing_one
+}
+
+filter_rf_matrix <- function(...) {
+  # Function body for filter_rf_matrix
+}
+
+find_flanking_markers <- function(...) {
+  # Function body for find_flanking_markers
+}
+
+est_hmm_map_biallelic_insert_marker <- function(...) {
+  # Function body for est_hmm_map_biallelic_insert_marker
+}
+
+mf_h <- function(...) {
+  # Function body for mf_h
+}
+
+parse_lg_and_type <- function(...) {
+  # Function body for parse_lg_and_type
+}
+
+get_markers_from_ordered_sequence <- function(...) {
+  # Function body for get_markers_from_ordered_sequence
+}
